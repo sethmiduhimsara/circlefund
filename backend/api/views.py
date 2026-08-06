adcc2ab3088df1cfbb1fd551d22109eb3fc637ef
Sethmindu Himsara <himsaragallewatta@gmail.com>
2026-08-06 10:19:13 +0530

    create first view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]

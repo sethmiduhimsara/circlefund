bb542ce8e04f79f9d939469dedc43af0bdd4896d
Sethmindu Himsara <himsaragallewatta@gmail.com>
2026-08-06 10:36:44 +0530

    create models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Circle(models.Model):
+    name = models.CharField(max_length=100)
+
+    invite_code = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        editable=False
+    )
+
+    admin = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="admin_circles"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
